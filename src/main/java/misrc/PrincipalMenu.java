25a74a40fb1573afe90bae9a2e12aa2abdb4b044
andhdezortega <andhdezortega@gmail.com>
2025-04-07 17:57:59 +0200

    mensaje del commit

---
+++ src/main/java/misrc/PrincipalMenu.java
@@ -0,0 +1,21 @@
+package misrc;
+
+import java.util.Scanner;
+
+
+public class PrincipalMenu {
+    public static void main (String[] args) {
+        Scanner sc = new Scanner(System.in);
+        while(true) {
+            System.out.println("[1] Sumar");
+            System.out.println("[2] Restar");
+            System.out.println("[3] Salir");
+
+          
+        }
+    
+    
+}
+            
+
+}
